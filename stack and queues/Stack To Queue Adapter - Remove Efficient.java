afe106ecba6e46fa002e9426a560b06b44881897
varunmalik94 <52308757+varunmalik94@users.noreply.github.com>
2022-01-24 22:50:04 +0530

    Create Stack To Queue Adapter - Remove Efficient.java

---
+++ stack and queues/Stack To Queue Adapter - Remove Efficient.java
@@ -0,0 +1,136 @@
+/*
+
+1. You are required to complete the code of our StackToQueueAdapter class. The class should mimic the behaviour of a Queue and implement FIFO semantic.
+2. Here is the list of functions that are written in the class
+    2.1. add -> Accepts new data if there is space available in the underlying array or 
+    print "Queue overflow" otherwise.
+    2.2. remove -> Removes and returns value according to FIFO, if available or print 
+    "Queue underflow" otherwise and return -1.
+    2.3. peek -> Returns value according to FIFO, if available or print "Queue 
+    underflow" otherwise and return -1.
+    2.4. size -> Returns the number of elements available in the queue.
+3. Input and Output is managed for you.
+
+Input Format
+Input is managed for you
+
+Output Format
+Output is managed for you
+
+Constraints
+Note -> remove, peek and size should work in constant time. add should work in linear time.
+
+Sample Input
+add 10
+add 20
+add 30
+add 40
+add 50
+remove
+remove
+add 60
+add 70
+peek
+remove
+peek
+remove
+peek
+remove
+peek
+remove
+peek
+remove
+peek
+remove
+quit
+
+Sample Output
+10
+20
+30
+30
+40
+40
+50
+50
+60
+60
+70
+70
+Queue underflow
+Queue underflow
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class StackToQueueAdapter {
+    Stack<Integer> mains;
+
+    public StackToQueueAdapter() {
+        mains = new Stack<>();
+    }
+
+    int size() {
+        return mains.size();
+    }
+
+    void add(int val) {
+        Stack<Integer> helpers = new Stack<>();
+        
+        // push everything from mainsto helpers
+        while (!mains.isEmpty())
+            helpers.push(mains.pop());
+        
+        mains.push(val);
+        
+        while (!helpers.isEmpty())
+            mains.push(helpers.pop());
+    }
+
+    int remove() {
+        if (mains.isEmpty()) {
+            System.out.println("Queue underflow");
+            return -1;
+        }
+        return mains.pop();
+    }
+
+    int peek() {
+        if (mains.isEmpty()) {
+            System.out.println("Queue underflow");
+            return -1;
+        }
+        return mains.peek();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StackToQueueAdapter qu = new StackToQueueAdapter();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("add")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if (str.startsWith("remove")) {
+        int val = qu.remove();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("peek")) {
+        int val = qu.peek();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(qu.size());
+      }
+      str = br.readLine();
+    }
+  }
+}
